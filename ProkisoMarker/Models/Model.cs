50271454833ff51c086cb9188aadb58baef05c32
migiright <migigit@yahoo.co.jp>
2018-05-07 00:34:15 +0900

    Add Problems into Model

--- ProkisoMarker/Models/Model.cs
+++ ProkisoMarker/Models/Model.cs
@@ -1,8 +1,30 @@
 ﻿using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace ProkisoMarker.Models
 {
 	public class Model : BindableBase, IModel
 	{
+		public ObservableCollection<Problem> Problems { get; } = new ObservableCollection<Problem>();
+
+		public Model()
+		{
+			Problems.CollectionChanged += Problems_CollectionChanged;
+		}
+
+		private void Problems_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			if (e.NewItems != null) {
+				foreach (var (p, i) in e.NewItems.Cast<Problem>().Select((p, i) => (p, i))) {
+					if (string.IsNullOrEmpty(p.Name)) {
+						p.Name = "p" + (i+Problems.Count-e.NewItems.Count);
+					}
+				}
+			}
+			foreach (var (p, i) in Problems.Select((p, i) => (p, i))) {
+				p.No = i;
+			}
+		}
 	}
 }
