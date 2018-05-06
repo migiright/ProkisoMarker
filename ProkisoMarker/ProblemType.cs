f1326d5812598c28f17e46dfb631a3d149d61412
migiright <migigit@yahoo.co.jp>
2018-05-07 00:31:34 +0900

    Add ProblemType

---
+++ ProkisoMarker/ProblemType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProkisoMarker
+{
+	public enum ProblemType
+	{
+		Basic,
+		Advanced,
+	}
+
+	public static class ProblemTypeExt
+	{
+		public static string ToDisplayName(this ProblemType self)
+		{
+			switch (self) {
+			case ProblemType.Basic:
+				return "基本";
+			case ProblemType.Advanced:
+				return "応用";
+			}
+			throw new ArgumentException($"self: {self.ToString()} は無効な値");
+		}
+	}
+}
