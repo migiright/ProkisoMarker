c3849a8bf87587c19813f46b483b3a1601ee34ee
migiright <migigit@yahoo.co.jp>
2018-08-19 17:01:42 +0900

    Add Compiler.

---
+++ ProkisoMarker/Compiler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProkisoMarker
+{
+	public enum Compiler
+	{
+		Vs2017Community,
+		Vs2017Professional,
+	}
+}
